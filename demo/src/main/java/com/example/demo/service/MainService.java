5fa2ddc1422a838f5a0ee3ab03094845afb97480
Sushil Dosi <sdosi@ancestry.com>
2019-08-18 23:10:36 -0600

    commit three mostly working solution

--- demo/src/main/java/com/example/demo/service/MainService.java
+++ demo/src/main/java/com/example/demo/service/MainService.java
@@ -13,7 +13,6 @@ public class MainService {
 
     }
 
-
     public String convert(String input) {
         if(input == null || input == "" || input.isEmpty()){
             return "Input cannot be empty";
@@ -21,11 +20,7 @@ public class MainService {
         try {
             BigDecimal number = new BigDecimal(input);
             number = number.setScale(2, RoundingMode.HALF_UP);
-
-            System.out.println(number);
-
             String numberStr = number.toString();
-
 
             String[] negativeSplitStr = numberStr.split("-");  //check for negative number
             boolean inputNumberSign =  false;
@@ -33,7 +28,6 @@ public class MainService {
             //if negativeSplitStr size greater than 1 then we have negative sign
             int splitPosition  = negativeSplitStr.length > 1 ? 1 : 0;
 
-            inputNumberSign = true;
             String[] numbers = negativeSplitStr[splitPosition].split("\\.");
             String inputNumber  = numbers[0];
             String inputNumberDecimal = numbers[1];
@@ -41,17 +35,24 @@ public class MainService {
             int count = inputNumber.length() / 3;  // number spacing logic
             int split = inputNumber.length() % 3;
 
+            char[] numArray = inputNumber.toCharArray();
+            StringBuffer result = new StringBuffer(negativeSplitStr.length > 1 ? "-" :"");
+            for(int i=0; i<split; i++) {
+                result.append(numArray[i]);
+            }
+            for(int j=split;j<numArray.length;j=j+3){
+                result.append(" ");
+                result.append(numArray[j] + "" +numArray[j+1] + "" +numArray[j+2]);
+            }
 
+            result.append("." + inputNumberDecimal);
 
+            return result.toString();
 
         }
         catch (NumberFormatException e) {
-            return e.getMessage();
+            return e.toString();
         }
-
-
-
-        return input;
     }
 
 }
